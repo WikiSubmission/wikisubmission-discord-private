49be7d638745e511970c7c59985d49bb6b601df2
Sarim <sarim.xs@gmail.com>
2025-05-27 03:19:56 -0600

    Health check

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,13 @@ import { Bot } from "./bot/client";
     }
 
     // [Server]
-    const server = http.createServer();
+    const server = http.createServer((req, res) => {
+        if (req.url === "/" || req.url === '/health' && req.method === 'GET') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ status: 'ok' }));
+        }
+    });
+
     server.listen(process.env.PORT || 8080);
 
     // [Bot]
