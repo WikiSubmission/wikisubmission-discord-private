402205e020e760c0d26eefb6c8831e715fc03233
Sarim <sarim.xs@gmail.com>
2025-05-27 22:18:00 -0600

    Update fall back response

--- src/utils/discord/stringify-roles.ts
+++ src/utils/discord/stringify-roles.ts
@@ -1,13 +1,13 @@
 import { GuildMember, PartialGuildMember } from "discord.js";
 
 export function stringifyRoles(context: GuildMember | PartialGuildMember | null): string {
-    if (!context) return '--';
+    if (!context) return 'None';
     if (context instanceof GuildMember || "roles" in context) {
         return context.roles?.cache
             ?.filter((i) => i.name !== '@everyone')
             ?.map((i) => `<@&${i.id}>`)
-            ?.join(', ') || '--'
+            ?.join(', ') || 'None'
     } else { 
-        return '--'
+        return 'None'
     }
 }
