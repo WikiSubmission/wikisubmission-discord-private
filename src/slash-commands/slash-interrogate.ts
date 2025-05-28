62d2a7633b8c666f44d6c074f5f5ab0731bb58f2
Sarim <sarim.xs@gmail.com>
2025-05-28 02:22:27 -0600

    Add commands

---
+++ src/slash-commands/slash-interrogate.ts
@@ -0,0 +1,152 @@
+import { EmbedBuilder } from 'discord.js';
+import { WSlashCommand } from '../types/w-slash-command';
+import { DateUtils } from '../utils/date-utils';
+import { stringifyName } from '../utils/discord/stringify-name';
+import { syncMember } from '../utils/discord/sync-member';
+import { getRole } from '../utils/discord/get-role';
+import { getChannel, getChannels } from '../utils/discord/get-channel';
+import { logError } from '../utils/log-error';
+
+export default function Command(): WSlashCommand {
+    return {
+        name: 'interrogate',
+        description: 'Require verification from a user',
+        options: [
+            {
+                name: 'user',
+                description: 'The user to verify',
+                type: 6,
+                required: true,
+            },
+        ],
+        access_control: 'MOD_AND_ABOVE',
+        execute: async (interaction) => {
+            try {
+                // [Fetch suspect]
+                const suspectID = interaction.options.get('user')?.value as string;
+                var suspect = interaction.guild?.members.cache.get(suspectID);
+                const reason = interaction.options.get('reason')?.value as string;
+                if (!suspectID || !suspect) {
+                    suspect = await interaction.guild?.members.fetch(suspectID);
+                    if (!suspect) {
+                        await interaction.reply({
+                            content: `User "<@${suspectID}>" not found`,
+                            flags: ['Ephemeral']
+                        });
+                        return;
+                    }
+                }
+                // [Fetch associated roles and channels]
+                const channels = getChannels(['verify', 'staff-log'])
+                const unverifiedRole = getRole('Unverified', interaction);
+                if (!channels) {
+                    await interaction.reply({
+                        content: `At least one channel is missing: verify, staff-log `,
+                        flags: ['Ephemeral']
+                    })
+                    return;
+                }
+
+                if (!unverifiedRole) {
+                    await interaction.reply({
+                        content: `Unverified role not found`,
+                        flags: ['Ephemeral']
+                    })
+                    return;
+                }
+
+                // [Jail the user]
+                try {
+                    await suspect.roles.add(unverifiedRole);
+                } catch (error) {
+                    await interaction.reply({
+                        content: `Failed to interrogate user "<@${suspectID}>" (permission/role error).`,
+                        flags: ['Ephemeral']
+                    });
+                    return;
+                }
+
+                // [Send alerts]
+                if (!reason?.includes('!testing')) {
+                    await channels["verify"].send({
+                        content: `<@${suspect.user.id}> **Please verify with a staff member to gain access to the server.**`,
+                        embeds: [
+                            new EmbedBuilder()
+                                .addFields(
+                                    {
+                                        name: 'User',
+                                        value: stringifyName(suspect),
+                                    },
+                                    {
+                                        name: 'Account Created',
+                                        value:
+                                            DateUtils.distanceFromNow(
+                                                suspect.user.createdTimestamp,
+                                            )
+                                    },
+                                )
+                                .setFooter({
+                                    text: `${interaction.user.username}`,
+                                    iconURL: interaction.user.displayAvatarURL(),
+                                })
+                                .setTimestamp(Date.now())
+                                .setThumbnail(suspect.displayAvatarURL())
+                                .setColor('DarkRed'),
+                        ],
+                    });
+                    await channels["staff-log"].send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setAuthor({
+                                    name: `${suspect.user.username} was sent to verification channel`,
+                                    iconURL: suspect.displayAvatarURL(),
+                                })
+                                .addFields(
+                                    {
+                                        name: 'User',
+                                        value:
+                                            stringifyName(suspect),
+                                    },
+                                    {
+                                        name: 'Account Created',
+                                        value:
+                                            DateUtils.distanceFromNow(
+                                                suspect.user.createdTimestamp,
+                                            ),
+                                    },
+                                )
+                                .setFooter({
+                                    text: `${interaction.user.username}`,
+                                    iconURL: interaction.user.displayAvatarURL(),
+                                })
+                                .setThumbnail(suspect.displayAvatarURL())
+                                .setTimestamp(Date.now())
+                                .setColor('DarkButNotBlack'),
+                        ],
+                    });
+                }
+                // [Move user out of VC, if they were in one]
+                if (suspect.voice.channel) {
+                    const JAIL_VC_CHANNEL = getChannel('Verify VC', 'voice', interaction);
+                    if (JAIL_VC_CHANNEL) {
+                        await suspect.voice.setChannel(JAIL_VC_CHANNEL, 'Automoved (verification)');
+                    }
+                }
+                // [Update DB]
+                await syncMember(interaction.member as any);
+                // [Reply]
+                await interaction.reply({
+                    content: `✅ Unverified <@${suspectID}>. To add them back, use /verify command.`,
+                    flags: ['Ephemeral']
+                });
+            } catch (error) {
+                await interaction.reply({
+                    content: `\`Internal Server Error\``,
+                    flags: ['Ephemeral']
+                });
+                logError(error, 'slash-interrogate');
+                return;
+            }
+        },
+    };
+}
---
+++ src/slash-commands/slash-verify.ts
@@ -0,0 +1,117 @@
+import { EmbedBuilder } from 'discord.js';
+import { WSlashCommand } from '../types/w-slash-command';
+import { getChannels } from '../utils/discord/get-channel';
+import { getRole } from '../utils/discord/get-role';
+import { syncMember } from '../utils/discord/sync-member';
+import { logError } from '../utils/log-error';
+
+export default function Command(): WSlashCommand {
+  return {
+    name: 'verify',
+    description: 'Verify a user',
+    options: [
+      {
+        name: 'user',
+        description: 'The user to verify',
+        type: 6,
+        required: true,
+      },
+    ],
+    access_control: 'MOD_AND_ABOVE',
+    execute: async (interaction) => {
+      try {
+        // [Fetch suspect]
+        const suspectID = interaction.options.get('user')?.value as string;
+        var suspect = interaction.guild?.members.cache.get(suspectID);
+        if (!suspectID || !suspect) {
+          suspect = await interaction.guild?.members.fetch(suspectID);
+          if (!suspect) {
+            await interaction.reply({
+              content: `User "<@${suspectID}>" not found`,
+              flags: ['Ephemeral']
+            });
+            return;
+          }
+        }
+
+        // [Fetch associated roles and channels]
+        const channels = getChannels(['verify', 'staff-log'])
+        const jailRole = getRole('Unverified', interaction);
+        if (!channels) {
+          await interaction.reply({
+            content: `At least one channel is missing: verify, staff-log `,
+            flags: ['Ephemeral']
+          })
+          return;
+        }
+
+        if (!jailRole) {
+          await interaction.reply({
+            content: `Verified role not found`,
+            flags: ['Ephemeral']
+          })
+          return;
+        }
+
+        // [Unjail the user]
+        try {
+          await suspect.roles.remove(jailRole);
+        } catch (error) {
+          await interaction.reply({
+            content: `Failed to verify user "<@${suspectID}>" (permission/role error).`,
+            flags: ['Ephemeral']
+          });
+          return;
+        }
+
+        // [Send alerts]
+        await channels["verify"].send({
+          embeds: [
+            new EmbedBuilder()
+              .setAuthor({
+                name: `${suspect.user.username} verified`,
+                iconURL: suspect.displayAvatarURL(),
+              })
+              .setFooter({
+                text: `${interaction.user.username}`,
+                iconURL: interaction.user.displayAvatarURL(),
+              })
+              .setTimestamp(Date.now())
+              .setColor('DarkGreen'),
+          ],
+        });
+
+        await channels["staff-log"].send({
+          embeds: [
+            new EmbedBuilder()
+              .setAuthor({
+                name: `${suspect.user.username} verified`,
+                iconURL: suspect.displayAvatarURL(),
+              })
+              .setFooter({
+                text: `${interaction.user.username}`,
+                iconURL: interaction.user.displayAvatarURL(),
+              })
+              .setTimestamp(Date.now())
+              .setColor('DarkGreen'),
+          ],
+        });
+
+        // [Update DB]
+        await syncMember(interaction.member as any);
+
+        // [Reply]
+        await interaction.reply({
+          content: `✅ Verified <@${suspectID}>.`,
+          flags: ['Ephemeral']
+        });
+      } catch (error) {
+        await interaction.reply({
+          content: `\`Internal Server Error\``,
+          flags: ['Ephemeral']
+        });
+        logError(error, 'slash-verify');
+      }
+    },
+  };
+}
