f59a7fb0b11c7a5ca0e26650efb3a321921e4693
Sarim <sarim.xs@gmail.com>
2025-06-01 20:54:28 -0600

    Add VC join/leave listener

---
+++ src/event-listeners/voice-status-update.ts
@@ -0,0 +1,31 @@
+import { WEventListener } from "../types/w-event-listener";
+import { getChannel } from "../utils/discord/get-channel";
+import { logError } from "../utils/log-error";
+
+export default function listener(): WEventListener {
+    return {
+        name: "voiceStateUpdate",
+        handler: async (previousState, newState) => {
+            try {
+                // [Joined]
+                if (previousState.channelId === null && newState.channelId !== null) {
+                    const associatedTextChannel = getChannel(`${newState.channel?.name?.split(" ")[0].toLowerCase().trim()}-chat`);
+
+                    if (associatedTextChannel) {
+                        associatedTextChannel.send(`<@${newState.id}> has joined <#${newState.channelId}>.`);
+                    }
+                }
+
+                // [Left]
+                if (previousState.channelId !== null && newState.channelId === null) {
+                    const associatedTextChannel = getChannel(`${previousState.channel?.name?.split(" ")[0].toLowerCase().trim()}-chat`);
+                    if (associatedTextChannel) {
+                        associatedTextChannel.send(`\`${previousState.member?.displayName}\` has left <#${previousState.channelId}>.`);
+                    }
+                }
+            } catch (error) {
+                logError(error, __filename);
+            }
+        }
+    }
+}
