0711ee350bd47e787da876864f5f973f42cf7966
Sarim <sarim.xs@gmail.com>
2025-06-05 05:57:15 -0600

    Add debug log & trigger param

--- src/utils/discord/sync-member.ts
+++ src/utils/discord/sync-member.ts
@@ -1,24 +1,31 @@
 import { GuildMember, PartialGuildMember } from "discord.js";
 import { getSupabaseClient } from "../get-supabase-client";
+import { logError } from "../log-error";
 
-export async function syncMember(context: GuildMember | PartialGuildMember | null) {
-    if (!context || !("user" in context) || !("guild" in context)) return;
+export async function syncMember(context: GuildMember | PartialGuildMember | null, trigger?: string) {
+    try {
+        if (!context || !("user" in context) || !("guild" in context)) return;
 
-    const roleIds = context.roles?.cache
-        ?.filter(role => role.name !== '@everyone')
-        ?.map(role => role.id) || [];
+        const roleIds = context.roles?.cache
+            ?.filter(role => role.name !== '@everyone')
+            ?.map(role => role.id) || [];
 
-    await getSupabaseClient()
-        .from('DiscordMembers')
-        .upsert({
-            id: `${context.user.id}*${context.guild.id}`,
-            user_id: context.user.id,
-            user_name: context.user.username,
-            display_name: context.user.displayName || context.user.username || '--',
-            guild_id: context.guild.id,
-            joined_timestamp: Date.now(),
-            created_timestamp: context.user.createdTimestamp,
-            avatar_url: context.user.displayAvatarURL(),
-            roles: roleIds.join(','),
-        });
+        await getSupabaseClient()
+            .from('DiscordMembers')
+            .upsert({
+                id: `${context.user.id}*${context.guild.id}`,
+                user_id: context.user.id,
+                user_name: context.user.username,
+                display_name: context.user.displayName || context.user.username || '--',
+                guild_id: context.guild.id,
+                joined_timestamp: Date.now(),
+                created_timestamp: context.user.createdTimestamp,
+                avatar_url: context.user.displayAvatarURL(),
+                roles: roleIds.join(','),
+            });
+
+        console.log(`[Member Sync] ${context.user.username} (${context.user.displayName})${trigger ? ` [Trigger: ${trigger}]` : ''}`);
+    } catch (error) {
+        logError(error, __filename);
+    }
 }
