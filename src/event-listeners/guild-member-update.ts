c8d696ae3d655ee998c8535f24647813c11602b0
Sarim <sarim.xs@gmail.com>
2025-06-07 04:37:09 -0600

    Add listener

---
+++ src/event-listeners/guild-member-update.ts
@@ -0,0 +1,22 @@
+import { WEventListener } from "../types/w-event-listener";
+import { logError } from "../utils/log-error";
+import { syncMember } from "../utils/discord/sync-member";
+
+export default function listener(): WEventListener {
+    return {
+        name: "guildMemberUpdate",
+        handler: async (oldMember, newMember) => {
+            try {
+                // [Check if roles were changed]
+                if (oldMember.roles.cache.size !== newMember.roles.cache.size
+                    ||
+                    !oldMember.roles.cache.every(role => newMember.roles.cache.has(role.id))
+                ) {
+                    await syncMember(newMember, 'roleUpdate');
+                }
+            } catch (error) {
+                logError(error, __filename);
+            }
+        },
+    };
+}
