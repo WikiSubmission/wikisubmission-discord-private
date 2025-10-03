ed16a69dc89c14c2f25541c427cf14db1001e443
hichem <hichem.toumi.2001@gmail.com>
2025-10-03 22:50:22 +0100

    added autocomplete to WSlashCommand

--- src/types/w-slash-command.ts
+++ src/types/w-slash-command.ts
@@ -1,16 +1,23 @@
 import {
-    CacheType,
-    ChatInputApplicationCommandData,
-    ChatInputCommandInteraction,
-    CommandInteraction,
-    MessageContextMenuCommandInteraction,
-    RESTPostAPIChatInputApplicationCommandsJSONBody,
-    UserContextMenuCommandInteraction,
-} from 'discord.js';
-import { WAccessControlCategories } from './w-access-control-categories';
+  AutocompleteInteraction,
+  CacheType,
+  ChatInputApplicationCommandData,
+  ChatInputCommandInteraction,
+  CommandInteraction,
+  MessageContextMenuCommandInteraction,
+  RESTPostAPIChatInputApplicationCommandsJSONBody,
+  UserContextMenuCommandInteraction,
+} from "discord.js";
+import { WAccessControlCategories } from "./w-access-control-categories";
 
 export type WSlashCommand = RESTPostAPIChatInputApplicationCommandsJSONBody & {
-    execute: (interaction: ChatInputCommandInteraction<CacheType> | MessageContextMenuCommandInteraction<CacheType> | UserContextMenuCommandInteraction) => Promise<void>;
-    access_control?: WAccessControlCategories;
-    disabled_in_dm?: boolean;
+  execute: (
+    interaction:
+      | ChatInputCommandInteraction<CacheType>
+      | MessageContextMenuCommandInteraction<CacheType>
+      | UserContextMenuCommandInteraction,
+  ) => Promise<void>;
+  access_control?: WAccessControlCategories;
+  disabled_in_dm?: boolean;
+  autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
 };
