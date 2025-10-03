85ff20838d50cb91686a1b9278ac7576c0b0b212
hichem <hichem.toumi.2001@gmail.com>
2025-10-03 22:48:55 +0100

    changed dev server id

--- src/constants/supported-guilds.ts
+++ src/constants/supported-guilds.ts
@@ -1,8 +1,8 @@
-export const SupportedGuilds = { 
-    Production: {
-        id: "576134563496198144"
-    },
-    Development: {
-        id: "1295537396703690904"
-    }
-}
+export const SupportedGuilds = {
+  Production: {
+    id: "576134563496198144",
+  },
+  Development: {
+    id: "1372691917590237235", //"1295537396703690904"
+  },
+};
