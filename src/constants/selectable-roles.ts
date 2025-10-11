4f8f5888aaffb70d624aa277843c3b12d38b65c5
hichem <hichem.toumi.2001@gmail.com>
2025-10-11 02:45:28 +0100

    added torah study ping

--- src/constants/selectable-roles.ts
+++ src/constants/selectable-roles.ts
@@ -1,89 +1,95 @@
 export const SelectableRoles: {
-    category:
-      | 'Religion'
-      | 'Region'
-      | 'Age'
-      | 'Gender'
-      | 'Marital Status'
-      | 'Other Languages'
-      | 'Reminder Roles';
-    roleNames: string[];
-    allowMultiple?: boolean;
-  }[] = [
-    {
-      category: 'Religion',
-      roleNames: [
-        'Submitter',
-        'Traditional Muslim - Sunni',
-        'Traditional Muslim - Shia',
-        'Traditional Muslim - Other',
-        'Quranist',
-        'Christian',
-        'Christian - Nontrinitarian',
-        'Jewish',
-        'Buddhist',
-        'Hindu',
-        'Bahai',
-        'Sikh',
-        'Agnostic / Atheist',
-        'Undecided / Exploring',
-        'Other Religion',
-      ],
-    },
-    {
-      category: 'Age',
-      roleNames: ['13-17', '18-24', '25-29', '30-39', '40+'],
-    },
-    {
-      category: 'Region',
-      roleNames: [
-        'United States',
-        'Canada',
-        'South America',
-        'Europe',
-        'Middle East',
-        'Asia',
-        'Africa',
-        'Australia',
-      ],
-    },
-    {
-      category: 'Gender',
-      roleNames: ['Male', 'Female'],
-    },
-    {
-      category: 'Marital Status',
-      roleNames: ['Single', 'Married'],
-    },
-    {
-      category: 'Other Languages',
-      roleNames: [
-        'Turkish',
-        'Persian',
-        'French',
-        'Albanian',
-        'Spanish',
-        'Bengali',
-        'Arabic',
-        'Urdu',
-        'Swedish',
-        'German',
-        'Bahasa',
-        'Kurdish',
-        'Hebrew',
-        'Hausa',
-        'Hindi',
-        'Russian',
-        'Ukranian',
-        'Bosnian',
-        'Tamil',
-      ],
-      allowMultiple: true,
-    },
-    {
-      category: 'Reminder Roles',
-      roleNames: ['Recitation Ping', 'Quran Study Ping', 'Meditation Ping', 'VC Ping', 'Bumper'],
-      allowMultiple: true,
-    },
-  ];
-  
+  category:
+    | 'Religion'
+    | 'Region'
+    | 'Age'
+    | 'Gender'
+    | 'Marital Status'
+    | 'Other Languages'
+    | 'Reminder Roles'
+  roleNames: string[]
+  allowMultiple?: boolean
+}[] = [
+  {
+    category: 'Religion',
+    roleNames: [
+      'Submitter',
+      'Traditional Muslim - Sunni',
+      'Traditional Muslim - Shia',
+      'Traditional Muslim - Other',
+      'Quranist',
+      'Christian',
+      'Christian - Nontrinitarian',
+      'Jewish',
+      'Buddhist',
+      'Hindu',
+      'Bahai',
+      'Sikh',
+      'Agnostic / Atheist',
+      'Undecided / Exploring',
+      'Other Religion',
+    ],
+  },
+  {
+    category: 'Age',
+    roleNames: ['13-17', '18-24', '25-29', '30-39', '40+'],
+  },
+  {
+    category: 'Region',
+    roleNames: [
+      'United States',
+      'Canada',
+      'South America',
+      'Europe',
+      'Middle East',
+      'Asia',
+      'Africa',
+      'Australia',
+    ],
+  },
+  {
+    category: 'Gender',
+    roleNames: ['Male', 'Female'],
+  },
+  {
+    category: 'Marital Status',
+    roleNames: ['Single', 'Married'],
+  },
+  {
+    category: 'Other Languages',
+    roleNames: [
+      'Turkish',
+      'Persian',
+      'French',
+      'Albanian',
+      'Spanish',
+      'Bengali',
+      'Arabic',
+      'Urdu',
+      'Swedish',
+      'German',
+      'Bahasa',
+      'Kurdish',
+      'Hebrew',
+      'Hausa',
+      'Hindi',
+      'Russian',
+      'Ukranian',
+      'Bosnian',
+      'Tamil',
+    ],
+    allowMultiple: true,
+  },
+  {
+    category: 'Reminder Roles',
+    roleNames: [
+      'Recitation Ping',
+      'Quran Study Ping',
+      'Torah & Gospel Study Ping',
+      'Meditation Ping',
+      'VC Ping',
+      'Bumper',
+    ],
+    allowMultiple: true,
+  },
+]
