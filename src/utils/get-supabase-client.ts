8f524bfafac4a2b06392f90b02745abe1d580cd1
Sarim <sarimyx@proton.me>
2025-11-10 07:35:42 -0700

    force unwrap supabase keys

--- src/utils/get-supabase-client.ts
+++ src/utils/get-supabase-client.ts
@@ -2,14 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/generated/database.types";
 
 export function getSupabaseInternalClient() {
-  if (!process.env.SUPABASE_URL) return null;
-
-  const key =
-    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY;
-
-  if (!key) return null;
-
-  return createClient<Database, "internal">(process.env.SUPABASE_URL, key, {
+  return createClient<Database, "internal">(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY!, {
     db: {
       schema: "internal",
     },
