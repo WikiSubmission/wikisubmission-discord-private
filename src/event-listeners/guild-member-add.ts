0a542ee57172eb7d93bd5eb025c1347814bcf642
Sarim <sarimyx@proton.me>
2025-11-11 02:59:33 -0700

    remove welcome embed (removed)

--- src/event-listeners/guild-member-add.ts
+++ src/event-listeners/guild-member-add.ts
@@ -38,27 +38,6 @@ export default function listener(): WEventListener {
 
         // [Case: member not found i.e. newly joined]
         if (!memberRecord.data) {
-          // [Welcome]
-          channels["welcome"].send({
-            content: `<@${member.user.id}>`,
-            embeds: [
-              new EmbedBuilder()
-                .setTitle("Peace be upon you!")
-                .setThumbnail(member.displayAvatarURL())
-                .setColor("DarkBlue")
-                .setTimestamp(Date.now())
-                .setDescription(
-                  `**Welcome to the Submission Server,** ${member.user.displayName || member.user.username}. Feel free to look around and check out the different channels.\n\nThis server is most active with voice chat activity. Join <#${getChannel("VC1", "voice", member)?.id || "576134569338732565"}> any time to get involved in discussions, ask questions, share your thoughts, or just listen in.\n\nYou can choose your server-roles at <#${channels["choose-roles"].id}>.`
-                )
-                .setFooter({
-                  text:
-                    member.guild.memberCount % 19 === 0
-                      ? `Members: ${member.guild.memberCount} (19 x ${Math.floor(member.guild.memberCount / 19)}) • ${member.user.username}`
-                      : `Members: ${member.guild.memberCount} • ${member.user.username}`,
-                }),
-            ],
-          });
-
           // [Staff notice]
           const rolesString = stringifyRoles(member);
           channels["admissions"].send({
@@ -100,27 +79,6 @@ export default function listener(): WEventListener {
 
         // [Case: member found i.e. rejoined]
         else {
-          // [Welcome message]
-          await channels["welcome"].send({
-            content: `<@${member.user.id}>`,
-            embeds: [
-              new EmbedBuilder()
-                .setTitle("Peace be upon you!")
-                .setThumbnail(member.displayAvatarURL())
-                .setColor("DarkBlue")
-                .setTimestamp(Date.now())
-                .setDescription(
-                  `**Welcome back,** <@${member.user.id}>. Join <#${getChannel("VC1", "text", member)?.id || "576134569338732565"}> any time to get involved in discussions, ask questions, share your thoughts, or just listen in.\n\nYou can choose your server-roles at <#${getChannel("choose-roles", "text", member)?.id || "576134569338732565"}>.`
-                )
-                .setFooter({
-                  text:
-                    member.guild.memberCount % 19 === 0
-                      ? `Members: ${member.guild.memberCount} (19 x ${Math.floor(member.guild.memberCount / 19)}) • ${member.user.username}`
-                      : `Members: ${member.guild.memberCount} • ${member.user.username}`,
-                }),
-            ],
-          });
-
           // [Staff notice]
           await channels["admissions"].send({
             embeds: [
