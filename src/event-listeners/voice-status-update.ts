99d2c64c1cc7926e8facd9dc17774bac77fe103c
hichem <hichem.toumi.2001@gmail.com>
2026-02-25 01:33:35 +0100

    feat: added jail rote to exclusion list

--- src/event-listeners/voice-status-update.ts
+++ src/event-listeners/voice-status-update.ts
@@ -19,6 +19,7 @@ export default function listener(): WEventListener {
         if (!member || member.user.bot) return;
 
         const inVcRole = getRole("IN VC", newState.guild);
+        const jail = getRole("Jail", newState.guild);
         if (!inVcRole) return;
 
         const newChannel = newState.channel;
@@ -26,7 +27,10 @@ export default function listener(): WEventListener {
           newChannel?.name.startsWith("Jail") ||
           newChannel?.name.startsWith("Verify");
 
-        const shouldHaveRole = newChannel !== null && !isInRestrictedChannel;
+        const shouldHaveRole =
+          newChannel !== null &&
+          !isInRestrictedChannel &&
+          !member.roles.cache.has(jail.id);
 
         if (shouldHaveRole) {
           if (!member.roles.cache.has(inVcRole.id)) {
